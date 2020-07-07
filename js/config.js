548154ca6a250654f24b33c3bf216cdca2139210
Mukesh Yadav <mukeshyadav@gmail.com>
2020-07-07 13:25:52 +0530

    integration of aws cognito

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
-    cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
-    },
-    api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
-    }
+  cognito: {
+    userPoolId: "ap-south-1_OhzwadPMc", // e.g. us-east-2_uXboG5pAb
+    userPoolClientId: "27rj7cn3ee2101otebitu7vb5b", // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+    region: "ap-south-1" // e.g. us-east-2
+  },
+  api: {
+    invokeUrl: "" // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+  }
 };
